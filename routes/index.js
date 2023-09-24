b5cd27b883031efa699829bfae0dacc3a6f55e57
will sherman <willsherman771@gmail.com>
2023-09-23 20:17:59 -0400

    still need to kill bugs

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const apiRoutes = require('./api'); // Includes all the api related routes
+const homeRoutes = require('./homeRoutes'); // Include all home related routes
+
+// Define the routes usage
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+
+module.exports = router;
+
